1461cb0e7c564e260d0816bf8993bd1cab493487
syermakov <hekate.dev@gmail.com>
2017-04-14 22:49:04 +0300

    Fixed issue with invalid logger category being used by NetworkMessagingClient.

--- hekate-core/src/main/java/io/hekate/messaging/internal/NetworkMessagingClient.java
+++ hekate-core/src/main/java/io/hekate/messaging/internal/NetworkMessagingClient.java
@@ -29,7 +29,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 class NetworkMessagingClient<T> implements MessagingClient<T> {
-    private static final Logger log = LoggerFactory.getLogger(MessagingGateway.class);
+    private static final Logger log = LoggerFactory.getLogger(NetworkMessagingClient.class);
 
     private static final boolean DEBUG = log.isDebugEnabled();
 
