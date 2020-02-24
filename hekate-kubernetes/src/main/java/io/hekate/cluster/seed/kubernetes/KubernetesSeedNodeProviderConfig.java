4ec37727d5d1463fab6834ccebedb0059667c62c
syermakov <hekate.dev@gmail.com>
2020-02-24 16:17:56 +0300

    Update copyright year.

--- hekate-kubernetes/src/main/java/io/hekate/cluster/seed/kubernetes/KubernetesSeedNodeProviderConfig.java
+++ hekate-kubernetes/src/main/java/io/hekate/cluster/seed/kubernetes/KubernetesSeedNodeProviderConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 The Hekate Project
+ * Copyright 2020 The Hekate Project
  *
  * The Hekate Project licenses this file to you under the Apache License,
  * version 2.0 (the "License"); you may not use this file except in compliance
@@ -227,7 +227,7 @@ public class KubernetesSeedNodeProviderConfig {
      *
      * @return This instance.
      */
-    public KubernetesSeedNodeProviderConfig withTrustCertificates(Boolean trustCertificates) { 
+    public KubernetesSeedNodeProviderConfig withTrustCertificates(Boolean trustCertificates) {
         setTrustCertificates(trustCertificates);
 
         return this;
